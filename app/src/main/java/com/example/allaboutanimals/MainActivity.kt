ae17463eedb838523f7f0085158e2ce08d4d0333
Kashif Mehmood <kashismails@gmail.com>
2020-08-02 11:58:57 +0500

    initial commit

---
+++ app/src/main/java/com/example/allaboutanimals/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.allaboutanimals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
